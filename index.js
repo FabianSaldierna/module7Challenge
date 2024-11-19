796e72d0dfc4276b8c6ce0b645c375558e393fbf
FabianSaldierna <fsaldierna@banxico.org.mx>
2024-11-18 22:02:36 -0600

    added the required questions, using inquirer, for this challenge

--- index.js
+++ index.js
@@ -1,13 +1,84 @@
 // TODO: Include packages needed for this application
-
+import inquirer from 'inquirer';
+import fs from 'fs';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: "Enter the name of your project",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter a description of your project",
+    },
+    {
+        type: 'input',
+        name: 'installationInstr',
+        message: "Enter installation instruction of your project",
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: "Enter usage Information of your project",
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: "Enter contribution guidelines of your project",
+    },
+    {
+        type: 'input',
+        name: 'testInstr',
+        message: "Enter test Instrucions of your project",
+    },
+    {
+        type: 'list',
+        message:(
+            'Choose a licencse:'
+        ),
+        name: 'license',
+        choices: ['apache', 'GNU', 'MIT'],
+    },
+    {
+        type: 'input',
+        name: 'gitHubAcc',
+        message: "Enter your GitHub account",
+    },
+    {
+        type: 'input',
+        name: 'emailAddress',
+        message: "Enter your email address",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let textoLicencia;
+    if (data.license == "apache") {
+        textoLicencia = "Licencia apache";
+    }if (data.license =="GNU"){
+        textoLicencia = "Licencia GNU";
+    } if (data.license == "MIT") {
+        textoLicencia = "Licencia MIT";
+}
 
+    const texto =
+`Ejemplo de texto
+${data.projectTitle}
+bye`;
+    fs.writeFile(fileName, texto, (err) =>
+    err ? console.log(err) : console.log('Success!'));
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+        writeToFile('salida2.txt', answers);
+        //console.log(answers.projectTitle);
+    });
+}
 
 // Function call to initialize app
 init();
